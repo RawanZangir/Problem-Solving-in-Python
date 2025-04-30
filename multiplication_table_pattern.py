6bd181393ac8754f28304498ece7e545087ce816
RawanZangir <153962410+RawanZangir@users.noreply.github.com>
2025-04-30 18:47:47 +0300

    Update multiplication_table_pattern.py

--- multiplication_table_pattern.py
+++ multiplication_table_pattern.py
@@ -1,5 +1,28 @@
-# Display a triangle-style multiplication table up to 5
+# Display a multiplication table up to 5
 
 for i in range(1, 6):
     for j in range(1, i+1):
         print(f"{i}x{j}={i*j}")
+        
+# Create a list of lists for a multiplication table
+
+x = int(input("Enter a number "))
+table = [] 
+
+for i in range(1, x+1):
+    y = []  
+    for j in range(1, x+1):
+        if j <= i:
+            y.append(i * j)
+        else:
+            break
+    table.append(y)
+
+print(table)
+
+
+
+     
+
+
+
