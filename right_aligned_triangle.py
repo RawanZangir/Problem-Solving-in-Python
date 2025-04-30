2c27f9c8a93748d3b4411a8af1b1c3c372e65c17
RawanZangir <153962410+RawanZangir@users.noreply.github.com>
2025-04-30 18:39:08 +0300

    Update right_aligned_triangle.py

--- right_aligned_triangle.py
+++ right_aligned_triangle.py
@@ -4,4 +4,15 @@ x = 6
 for i in range(1, x + 1):
  
  print(' ' * (x - i) + '*' * i)
-    
+ 
+# A right-aligned triangle pattern using asterisks with lists   
+x = 5
+pyramid = []
+row = [' '] * x  
+
+for i in range(x):
+    row.pop(0)        
+    row.append('*')   
+    print(row)
+
+   
