1f914e002a1464e0a22d9f82a1d62c3e62969d7a
RawanZangir <153962410+RawanZangir@users.noreply.github.com>
2025-05-03 20:02:58 +0300

    Create module.py

---
+++ module.py
@@ -0,0 +1,15 @@
+import sort-func
+import pyramid-func
+import multiplication-func
+
+multiplication-func.multiplication(multiplication-func.x) 
+
+
+print("Pyramid:")
+pyramid-func.pyramid(5)
+
+
+print("Sorted numbers:")
+ascending, descending = sort-func.sort()
+print("Ascending:", ascending)
+print("Descending:", descending)
